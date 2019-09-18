969ed704b7744aab55403bd032a1563e35ff174f
Rob Anderson <randerson1@companieshouse.gov.uk>
2019-09-18 15:08:52 +0100

    change tabs to spaces and remove system out

--- src/main/java/uk/gov/companieshouse/processor/FilingProcessorImpl.java
+++ src/main/java/uk/gov/companieshouse/processor/FilingProcessorImpl.java
@@ -16,36 +16,35 @@ import uk.gov.companieshouse.filing.received.FilingReceived;
 @Component
 public class FilingProcessorImpl implements FilingProcessor {
 
-	private static final String ACCEPTED = "accepted";
-	private static final String REJECTED = "rejected";
+    private static final String ACCEPTED = "accepted";
+    private static final String REJECTED = "rejected";
 
-	@Override
-	public FilingProcessed process(FilingReceived filingReceived) {
-		System.out.println("Processing " + filingReceived.getApplicationId());
-		FilingProcessed processed = new FilingProcessed();
-		processed.setApplicationId(filingReceived.getApplicationId());
-		processed.setAttempt(1);
-		processed.setChannelId(filingReceived.getChannelId());
-		PresenterRecord presenterRecord = new PresenterRecord();
-		presenterRecord.setLanguage(filingReceived.getPresenter().getLanguage());
-		presenterRecord.setUserId(filingReceived.getPresenter().getUserId());
-		processed.setPresenter(presenterRecord);
-		processed.setResponse(createResponse(filingReceived));
-		SubmissionRecord submissionRecord = new SubmissionRecord();
-		submissionRecord.setTransactionId(filingReceived.getSubmission().getTransactionId());
-		processed.setSubmission(submissionRecord);
-		return processed;
-	}
+    @Override
+    public FilingProcessed process(FilingReceived filingReceived) {
+        FilingProcessed processed = new FilingProcessed();
+        processed.setApplicationId(filingReceived.getApplicationId());
+        processed.setAttempt(1);
+        processed.setChannelId(filingReceived.getChannelId());
+        PresenterRecord presenterRecord = new PresenterRecord();
+        presenterRecord.setLanguage(filingReceived.getPresenter().getLanguage());
+        presenterRecord.setUserId(filingReceived.getPresenter().getUserId());
+        processed.setPresenter(presenterRecord);
+        processed.setResponse(createResponse(filingReceived));
+        SubmissionRecord submissionRecord = new SubmissionRecord();
+        submissionRecord.setTransactionId(filingReceived.getSubmission().getTransactionId());
+        processed.setSubmission(submissionRecord);
+        return processed;
+    }
 
-	private ResponseRecord createResponse(FilingReceived filingReceived) {
-		ResponseRecord response = new ResponseRecord();
-		response.setCompanyName(filingReceived.getSubmission().getCompanyName());
-		response.setCompanyNumber(filingReceived.getSubmission().getCompanyNumber());
-		response.setDateOfCreation("2019-09-11T13:02:36Z"); // todo now ? this is the format required
-		response.setProcessedAt("2019-09-11T13:02:36Z"); // todo now ? this is the format required
-		response.setStatus(ACCEPTED);
-		response.setSubmissionId(filingReceived.getItems().get(0).getSubmissionId()); // get first item
-		return response;
-	}
+    private ResponseRecord createResponse(FilingReceived filingReceived) {
+        ResponseRecord response = new ResponseRecord();
+        response.setCompanyName(filingReceived.getSubmission().getCompanyName());
+        response.setCompanyNumber(filingReceived.getSubmission().getCompanyNumber());
+        response.setDateOfCreation("2019-09-11T13:02:36Z"); // todo now ? this is the format required
+        response.setProcessedAt("2019-09-11T13:02:36Z"); // todo now ? this is the format required
+        response.setStatus(ACCEPTED);
+        response.setSubmissionId(filingReceived.getItems().get(0).getSubmissionId()); // get first item
+        return response;
+    }
 
 }
