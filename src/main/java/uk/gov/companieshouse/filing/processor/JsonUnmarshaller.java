e8a247321a6e6d78c35459918989391be0cd8cd0
Rafa <rgarcia@companieshouse.gov.uk>
2020-02-19 16:55:11 +0000

    Add unit test

--- src/main/java/uk/gov/companieshouse/filing/processor/JsonUnmarshaller.java
+++ src/main/java/uk/gov/companieshouse/filing/processor/JsonUnmarshaller.java
@@ -1,7 +1,6 @@
 package uk.gov.companieshouse.filing.processor;
 
-import java.io.IOException;
-
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectReader;
@@ -19,7 +18,7 @@ public class JsonUnmarshaller implements Unmarshaller {
     private static final ObjectReader ADDRESS_READER = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false).readerFor(Address.class);
 
     @Override
-    public Address unmarshallAddress(String json) throws IOException {
+    public Address unmarshallAddress(String json) throws JsonProcessingException {
         return ADDRESS_READER.readValue(json);
     }
 
